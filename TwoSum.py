85f9b9c1767402ae629b05231c2b09f16792d908
Human001 <124699333+thachha901@users.noreply.github.com>
2025-06-18 23:34:06 +0700

    Day1

--- TwoSum.py
+++ TwoSum.py
@@ -5,8 +5,21 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
+        #O(N^2) complexity solution using brute force
         for i in range(len(nums)):
             for j in range (len(nums)):
                 if i < j and nums[i] + nums[j] == target:
                     return i, j
         
+        
+        
+        #O(n) complexity solution using a hash map
+        # num_map = {}
+        
+        # for i, num in enumerate(nums):
+        #     complement = target - num
+        #     if complement in num_map:
+        #         return [num_map[complement], i]
+        #     num_map[num] = i
+            
+        # return num_map
