5dc8c1b80bc01397e66fe190f4fb4cfd4bd32300
Human001 <124699333+thachha901@users.noreply.github.com>
2025-07-14 18:44:56 +0700

    Day17

---
+++ binaryToNum.py
@@ -0,0 +1,27 @@
+# # Definition for singly-linked list.
+# Given head which is a reference node to a singly-linked list. The value of each node in the linked list is either 0 or 1. The linked list holds the binary representation of a number.
+
+# Return the decimal value of the number in the linked list.
+
+# The most significant bit is at the head of the linked list.
+
+
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def getDecimalValue(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: int
+        """
+        val = 0
+        if head is None:
+            return 0
+        while head:
+            val = val * 2 + head.val
+            head = head.next
+
+        return val
+
