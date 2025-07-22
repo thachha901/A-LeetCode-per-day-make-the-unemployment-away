7dbd94dc2def0ac6ec8c8365206eef3528563a27
Human001 <124699333+thachha901@users.noreply.github.com>
2025-07-22 23:24:26 +0700

    Day25

---
+++ maxSubArray.py
@@ -0,0 +1,30 @@
+# You are given an array of positive integers nums and want to erase a subarray containing unique elements. The score you get by erasing the subarray is equal to the sum of its elements.
+
+# Return the maximum score you can get by erasing exactly one subarray.
+
+# An array b is called to be a subarray of a if it forms a contiguous subsequence of a, that is, if it is equal to a[l],a[l+1],...,a[r] for some (l,r).
+
+class Solution(object):
+    def maximumUniqueSubarray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        seen = set()
+        left = 0
+        current_sum = 0
+        max_sum = 0
+
+        for right in range(len(nums)):
+            while nums[right] in seen:
+                seen.remove(nums[left])
+                current_sum -= nums[left]
+                left += 1
+            
+            seen.add(nums[right])
+            current_sum += nums[right]
+            max_sum = max(current_sum, max_sum)
+
+        
+        return max_sum
+        
