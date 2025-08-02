c21c35a0a999d1e2d09ff74d95da663a5f5e2a2a
Human001 <124699333+thachha901@users.noreply.github.com>
2025-08-02 16:24:06 +0700

    Day35

---
+++ reversedWord.py
@@ -0,0 +1,21 @@
+# Given an input string s, reverse the order of the words.
+
+# A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.
+
+# Return a string of the words in reverse order concatenated by a single space.
+
+# Note that s may contain leading or trailing spaces or multiple spaces between two words. The returned string should only have a single space separating the words. Do not include any extra spaces.
+
+
+class Solution(object):
+    def reverseWords(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        
+        s = s.strip().split()
+
+        reversed_words = s[::-1]
+
+        return " ".join(reversed_words)
